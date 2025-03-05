3771ac56135c9d81fbd272274625ccb5a3ce09e7
Jimmy T. <jimmy.tang@my.ccsu.edu>
2025-03-05 18:15:48 -0500

    feat: added TestBench for main CPU module

---
+++ src/CPU_Testbench.v
@@ -0,0 +1,19 @@
+// Test module
+module CPU_Testbench ();
+  reg clock;
+  wire signed [15:0] wd, ir, pc;
+
+  // Instantiate CPU
+  CPU test_cpu(clock, pc, wd, ir);
+
+  //Clock Generation (Toggle every 1 time unit)
+  always #1 clock = ~clock;
+
+  //Test Execution
+  initial begin
+    $display ("Clock  PC    IR                                 WD");
+    $monitor ("%b     %2d   %b  %3d (%b)", clock, pc, ir, wd, wd);
+    clock = 1;
+    #16 $finish;
+  end
+endmodule
