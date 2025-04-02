ff55cd7bb7b7071e921ae8f09210b74a52e60c62
Jake M. <20688797+V-D-X@users.noreply.github.com>
2025-04-02 10:36:16 -0400

    feat: added more I-type instruction and expanded control format
    Extended the MainControl module to support LW, SW, BEQ, and BNE. Updated the control signal format to handle a wider range of instruction behaviors and execution requirements.

--- src/MainControl.v
+++ src/MainControl.v
@@ -3,21 +3,25 @@ module MainControl (op, control);
   input [3:0] op;  // 4-bit opcode from instruction field
 
   // Outputs:
-  output reg [6:0] control; // Control signals for CPU execution
+  output reg [10:0] control; // Control signals for CPU execution
+  // Format: reg_dst, alu_src, mem_to_reg, reg_write, mem_write, beq, bne, alu_ctl[3:0]
 
-  // Control signals: reg_dst, alu_src, reg_write, alu_ctl
-  always @(op) 
+  always @(op)
     case (op)
       // R-type instructions
-      4'b0000: control = 7'b1_0_1_0010;  // add  | rd, rs, rt | rd = rs + rt
-      4'b0001: control = 7'b1_0_1_0110;  // sub  | rd, rs, rt | rd = rs - rt
-      4'b0010: control = 7'b1_0_1_0000;  // and  | rd, rs, rt | rd = rs & rt
-      4'b0011: control = 7'b1_0_1_0001;  // or   | rd, rs, rt | rd = rs | rt
-      4'b0100: control = 7'b1_0_1_1100;  // nor  | rd, rs, rt | rd = ~(rs | rt)
-      4'b0101: control = 7'b1_0_1_1101;  // nand | rd, rs, rt | rd = ~(rs & rt)
-      4'b0110: control = 7'b1_0_1_0111;  // slt  | rd, rs, rt | rd = (rs < rt) ? 1 : 0
+      4'b0000: control = 11'b1_0_0_1_0_0_0_0010;  // add   | rd, rs, rt     | rd = rs + rt
+      4'b0001: control = 11'b1_0_0_1_0_0_0_0110;  // sub   | rd, rs, rt     | rd = rs - rt
+      4'b0010: control = 11'b1_0_0_1_0_0_0_0000;  // and   | rd, rs, rt     | rd = rs & rt
+      4'b0011: control = 11'b1_0_0_1_0_0_0_0001;  // or    | rd, rs, rt     | rd = rs | rt
+      4'b0100: control = 11'b1_0_0_1_0_0_0_1100;  // nor   | rd, rs, rt     | rd = ~(rs | rt)
+      4'b0101: control = 11'b1_0_0_1_0_0_0_1101;  // nand  | rd, rs, rt     | rd = ~(rs & rt)
+      4'b0110: control = 11'b1_0_0_1_0_0_0_0111;  // slt   | rd, rs, rt     | rd = (rs < rt) ? 1 : 0
 
       // I-type instructions
-      4'b0111: control = 7'b0_1_1_0010;  // addi | rd, rs, const8 | rd = rs + const8
+      4'b0111: control = 11'b0_1_0_1_0_0_0_0010;  // addi  | rd, rs, const8 | rd = rs + const8
+      4'b1000: control = 11'b0_1_1_1_0_0_0_0010;  // lw    | rd, off8(rs)   | rd = mem16[rs + off8]
+      4'b1001: control = 11'b0_1_0_0_1_0_0_0010;  // sw    | rs, off8(rt)   | mem16[rt + off8] = rs
+      4'b1010: control = 11'b0_0_0_0_0_1_0_0110;  // beq   | rs, rt, off8   | if (rs == rt) PC += off8
+      4'b1011: control = 11'b0_0_0_0_0_0_1_0110;  // bne   | rs, rt, off8   | if (rs != rt) PC += off8
     endcase
 endmodule
